ac97b94fbf1bf30ad941a0a8ea2c7fcd4c4d762e
Niidashi Kunihiro <nii.kunihiro@gmail.com>
2019-06-13 00:50:06 +0900

    add html samples

---
+++ src/Module/HtmlModule.php
@@ -0,0 +1,15 @@
+<?php
+namespace Nielsen\SelectRunner\Module;
+
+use Madapaja\TwigModule\TwigErrorPageModule;
+use Madapaja\TwigModule\TwigModule;
+use Ray\Di\AbstractModule;
+
+class HtmlModule extends AbstractModule
+{
+    protected function configure()
+    {
+        $this->install(new TwigModule);
+        $this->install(new TwigErrorPageModule);
+    }
+}
