1286c19f66c245649d9e4fff20a21ae9462a031e
Niidashi Kunihiro <nii.kunihiro@gmail.com>
2019-06-13 01:18:00 +0900

    add aura input modules

--- src/Module/AppModule.php
+++ src/Module/AppModule.php
@@ -4,6 +4,7 @@ namespace Nielsen\SelectRunner\Module;
 use BEAR\Package\AbstractAppModule;
 use BEAR\Package\PackageModule;
 use BEAR\Package\Provide\Router\AuraRouterModule;
+use Ray\WebFormModule\AuraInputModule;
 
 class AppModule extends AbstractAppModule
 {
@@ -15,6 +16,7 @@ class AppModule extends AbstractAppModule
         $appDir = $this->appMeta->appDir;
         require_once $appDir . '/env.php';
         $this->install(new AuraRouterModule($appDir . '/var/conf/aura.route.php'));
+        $this->install(new AuraInputModule);
         $this->install(new PackageModule);
     }
 }
