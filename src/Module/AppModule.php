2afb44d839c32cac5d85e21567e24455f8831f50
Niidashi Kunihiro <nii.kunihiro@gmail.com>
2019-06-14 01:42:45 +0900

    db-app-package の必要なものだけをインストール

--- src/Module/AppModule.php
+++ src/Module/AppModule.php
@@ -5,8 +5,10 @@ use BEAR\Package\AbstractAppModule;
 use BEAR\Package\PackageModule;
 use BEAR\Package\Provide\Router\AuraRouterModule;
 use Koriym\DbAppPackage\DbAppPackage;
+use Koriym\QueryLocator\QueryLocatorModule;
 use Nielsen\SelectRunner\Form\QueryForm;
-use Ray\Di\Scope;
+use Ray\AuraSqlModule\AuraSqlModule;
+use Ray\Query\SqlQueryModule;
 use Ray\WebFormModule\AuraInputModule;
 use Ray\WebFormModule\FormInterface;
 use SqlFormatter;
@@ -21,7 +23,18 @@ class AppModule extends AbstractAppModule
         $appDir = $this->appMeta->appDir;
         require_once $appDir . '/env.php';
         $this->install(new AuraRouterModule($appDir . '/var/conf/aura.route.php'));
-        $this->install(new DbAppPackage(getenv('DB_DSN'), getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_READ')));
+        // database
+        $this->install(
+            new AuraSqlModule(
+                getenv('DB_DSN'),
+                getenv('DB_USER'),
+                getenv('DB_PASS'),
+                getenv('DB_READ')
+            )
+        );
+        $this->install(new QueryLocatorModule($appDir . '/var/db/sql'));
+        $this->install(new SqlQueryModule($appDir . '/var/db/sql'));
+
         $this->bind(SqlFormatter::class);
         $this->install(new AuraInputModule);
         $this->bind(FormInterface::class)->annotatedWith('query_form')->to(QueryForm::class);
