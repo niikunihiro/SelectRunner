eaf06154fa36deb954a40d239ffaa313832cd259
niidashi <niidashi@fenrir.co.jp>
2019-06-13 11:42:13 +0900

    add  DbAppModule and remove router

--- src/Module/AppModule.php
+++ src/Module/AppModule.php
@@ -4,6 +4,7 @@ namespace Nielsen\SelectRunner\Module;
 use BEAR\Package\AbstractAppModule;
 use BEAR\Package\PackageModule;
 use BEAR\Package\Provide\Router\AuraRouterModule;
+use Koriym\DbAppPackage\DbAppPackage;
 use Ray\Di\Scope;
 use Ray\WebFormModule\AuraInputModule;
 use SqlFormatter;
@@ -18,6 +19,7 @@ class AppModule extends AbstractAppModule
         $appDir = $this->appMeta->appDir;
         require_once $appDir . '/env.php';
         $this->install(new AuraRouterModule($appDir . '/var/conf/aura.route.php'));
+        $this->install(new DbAppPackage(getenv('DB_DSN'), getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_READ')));
         $this->bind(SqlFormatter::class);
         $this->install(new AuraInputModule);
         $this->install(new PackageModule);
